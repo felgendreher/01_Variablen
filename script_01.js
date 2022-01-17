975ece0d1be840515f9696d866b1c36ed6813a57
felgendreher <felgendreher@gmail.com>
2022-01-17 10:38:58 +0100

    03a Konstanten

--- script_01.js
+++ script_01.js
@@ -3,5 +3,70 @@
 /***** Variablen 01 *******/
 
 
+// Zeilen-Kommentar zwei Slashes > Strg + #
+
+// Strg Shift p > Befehlzeile fürs Clonen
+
+/**
+ * 
+ * 
+ * Block-Kommentar
+ * 
+ * 
+ *  */
+
+
+// Konsolen-Ausgabe
+// console.log("Hello world")
+// console.log(firstname); // nicht deklarierte Var. --> Fehler
+
+/***** 02 Deklaration + Wertzuweisung I */
+
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
+
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung (value assignment)
+// console.log(familyName);
+
+// console.log(firstName + " " + familyName);
+
+// Zuweisung der Bezeichnung 02 Deklaration + Wertzuweisung I
+// und stage sowie commit nicht mitbekommen!
+
+/***** 03 Deklaration + Wertzuweisung II *****/
+
+// let firstName,familyName;
+// firstName = prompt("Bitte Vornamen eingeben:")
+// familyName = prompt("Bitte Nachnamen eingeben:")
+// console.log(firstName + " " + familyName);
+
+// JavaScript ist eine untypisierte Sprache! | untyped
+// typescript ist die Antwort auf dieses Verhalten!
+// let test;
+// test = "Hi";
+// // test = 2;
+// // test = true;
+
+// console.log("Datentype: " + typeof test)
+// console.log("Inhalt: " + test);
+
+
+/****** 03a Konstanten *****/
+
+const test = "Hi"; // Variable mit KONSTANTEN Inhalt
+test = 2; // KEINE neue Zuweisung zur LZ möglich
+console.log("Inhalt: " + test);
+
+
+
+
+
+
+
+
+
+
+
 
 
