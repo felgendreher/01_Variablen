13b87ffad077dc131a6eaae0d130ce8ff7c5fbfa
Peter <schmidt.multimedia@gmail.com>
2022-01-17 09:19:29 +0100

    02 Deklaration + Wertzuweisung I

--- script_01.js
+++ script_01.js
@@ -2,6 +2,24 @@
 
 /***** Variablen 01 *******/
 
+/** 
+ * 
+ *  Block-Kommentar
+ * 
+*/
 
+// Zeilen-Kommentar
 
+// Konsolen-Ausgabe
+// console.log("Hello World!");
+// console.log(firstName); // nicht deklarierte Var. --> Fehler
 
+/***** 02 Deklaration + Wertzuweisung I *******/
+
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); // Ausgabe
+
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log(familyName);
+console.log(firstName + " " + familyName);
