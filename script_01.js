45457f6e03e49f1acbaf2acc2daa9b8b1977ec51
Peter <schmidt.multimedia@gmail.com>
2022-01-17 10:27:27 +0100

    03 Deklaration + Wertzuweisung II

--- script_01.js
+++ script_01.js
@@ -16,10 +16,26 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName; // Deklaration (Definition)
-firstName = "Max"; // Wertzuweisung
-console.log(firstName); // Ausgabe
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
 
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log(familyName);
-console.log(firstName + " " + familyName);
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log(familyName);
+// console.log(firstName + " " + familyName);
+
+/***** 03 Deklaration + Wertzuweisung II *******/
+
+// let firstName,familyName;
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachnamen eingeben:");
+// console.log(firstName + " " + familyName);
+
+// JS ist eine untypisierte Sprache! | untyped
+let test;
+test = "Hi";
+test = 2;
+test = true;
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
